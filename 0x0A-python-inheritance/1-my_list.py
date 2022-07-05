c1545244120c78caaff572c6c03f585345ae4df8
selaml <selamawitlb@gmail.com>
2022-07-05 04:15:38 -0700

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines: class MyList"""
+
+
+class MyList(list):
+    """
+    Represents MyList class
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        initializes list
+        Arguments: None
+        Returns: None
+        """
+        super().__init__
+
+    def print_sorted(self):
+        """
+        prints a sorted list
+        Arguments:
+            None
+        Returns:
+            None
+        """
+
+        print(sorted(self))
