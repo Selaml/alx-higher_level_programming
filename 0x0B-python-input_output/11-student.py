b4c9fdbc5721fd6df34d93140682198e513df693
selaml <selamawitlb@gmail.com>
2022-07-05 23:11:33 -0700

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Module for Studen class"""
+# 11-student.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a class Student."""
 
 
 class Student:
-    """Class for student"""
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Constructor."""
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
-        if attrs is None:
-            return self.__dict__
-        dictionary = {}
-        for attr in attrs:
-            try:
-                dictionary[attr] = self.__dict__[attr]
-            except Exception:
-                pass
-        return dictionary
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
