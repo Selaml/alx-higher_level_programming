192255b89a300f7ad382867cbadc7a128d696609
selaml <selamawitlb@gmail.com>
2022-07-05 22:56:49 -0700

    student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,6 +10,19 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """retrieves dictionary"""
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for attr in attrs:
+            try:
+                dictionary[attr] = self.__dict__[attr]
+            except Exception:
+                pass
+        return dictionary
+
+    def reload_from_json(self, json):
+        """loads attributes from json"""
+        for key, value in json.items():
+            self.__dict__[key] = value
